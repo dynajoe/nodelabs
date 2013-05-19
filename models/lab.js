cfe306ac6b49d0aa2ab5a1b6d8e70218eed83731
Joe Andaverde <joe@andaverde.net>
2013-05-18 22:12:00 -0500

    added search icon

--- models/lab.js
+++ models/lab.js
@@ -1,11 +1,9 @@
+var data = require('../data/data')
+
 var lab = {
    search: function (params, callback) {
       callback(null, {
-         labs: [
-            { title: 'Introduction to Node.js', date: new Date(), description: 'An introduction to Node.js architecture and some examples.', duration: '1 hour' },
-            { title: 'Express.js', date: new Date(), description: 'How to create an express.js application.', duration: '20 min' },
-            { title: 'Socket.io', date: new Date(), description: 'Real-time web applications using socket.io.', duration: '15 min' },
-         ],
+         labs: data.labs,
          tags: ['intro', 'socket.io', 'express.js', 'real-time', 'jade']
       });
    }
